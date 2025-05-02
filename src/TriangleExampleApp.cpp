24f19ca8026fa8b319b442394715c1675f9f8ef9
Carlos Santos <carloshmfs.dev@pm.me>
2025-05-02 13:55:51 -0300

    feat: create a vulkan instance

--- src/TriangleExampleApp.cpp
+++ src/TriangleExampleApp.cpp
@@ -3,6 +3,13 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <vulkan/vulkan.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
 void TriangleExampleApp::run()
 {
     init_window();
@@ -21,7 +28,47 @@ void TriangleExampleApp::init_window()
 
 void TriangleExampleApp::init_vulkan()
 {
+    create_instance();
+}
 
+void TriangleExampleApp::create_instance()
+{
+    VkApplicationInfo appInfo {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &m_vk_instance);
+    (void)result;
+    if (vkCreateInstance(&createInfo, nullptr, &m_vk_instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create vk instance!");
+    }
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    std::cout << "available extensions:" << std::endl;
+
+    for (const auto& extension : extensions) {
+        std::cout << '\t' << extension.extensionName << std::endl;
+    }
 }
 
 void TriangleExampleApp::main_loop()
@@ -33,6 +80,7 @@ void TriangleExampleApp::main_loop()
 
 void TriangleExampleApp::cleanup()
 {
+    vkDestroyInstance(m_vk_instance, nullptr);
     glfwDestroyWindow(m_window);
     glfwTerminate();
 }
--- src/TriangleExampleApp.h
+++ src/TriangleExampleApp.h
@@ -1,9 +1,11 @@
 #pragma once
 
-#include <cstdio>
-
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
+
+#include <cstdlib>
 
 class TriangleExampleApp {
 public:
@@ -13,6 +15,9 @@ private:
     const uint32_t m_WIDTH = 800;
     const uint32_t m_HEIGHT = 600;
     GLFWwindow* m_window;
+
+    VkInstance m_vk_instance;
+    void create_instance();
 
     void init_window();
     void init_vulkan();
